e1f38aefdb7e6d6a159cb2c1afd461e47a8805e2
shengs <shery.sheng@emc.com>
2016-12-14 15:46:14 +0800

    first commit

---
+++ sparklearning.py
@@ -0,0 +1,11 @@
+import pyspark
+import os
+import sys
+
+if 'SPARK_HOME' not in os.environ:
+    os.environ['SPARK_HOME'] = '/srv/spark'
+
+    # Create a variable for our root path
+SPARK_HOME = os.environ['SPARK_HOME']
+
+print SPARK_HOME
